141155239d5d133230978bd74590ea8decdfcc42
”SKY123C“ <289079202@qq.com>
2026-06-16 11:20:56 +0800

    update

--- PythonFBX/package.py
+++ PythonFBX/package.py
@@ -8,9 +8,11 @@ build_command = ""
 
 def pre_build_commands():
     import os
-    #print(env.PATH)
-    env.FBXSDK_ROOT.append(os.path.join(build.build_path, "FBX/FBX SDK/2020.3.9"))
-    env.FBXSDK_COMPILER.append("vs2022")
     import subprocess
+    copy_env = os.environ.copy()
+    copy_env["FBXSDK_ROOT"] = os.path.join(build.build_path, "FBX/FBX SDK/2020.3.9")
+    copy_env["FBXSDK_COMPILER"] = "vs2022"
+    fbx_bindings_path = os.path.join(build.build_path, "FBX/FBX Python Bindings/2020.3.9")
     popen = subprocess.run(["python.exe", "-m", "pip", "install", "--force-reinstall", "-v", "sip==6.6.2"], check=True)
-    subprocess.run(["python.exe", "-m", "pip", "install", "--target=C:/PythonFBX", os.path.join(build.build_path, "FBX/FBX Python Bindings/2020.3.9")], check=True)
+    subprocess.run(["python.exe", "-m", "pip", "install", "--target=C:/PythonFBX", fbx_bindings_path], env=copy_env, check=True)
+
